6b99b1d691a90ccab02d4acbfa22bce109d97591
IIsqa <iismayilzade2006@gmail.com>
2024-08-31 11:22:32 +0400

    Commit mesaj

--- Task_3/app.py
+++ Task_3/app.py
@@ -1,51 +1,30 @@
+from flask import Flask, render_template, jsonify
 import pymysql
-from flask import Flask
 
-app= Flask(__name__)
+app = Flask(__name__)
 
-blog_list= [
-        {
-        "title": "Blog 1",
-        "content" : "content 1",
-        "author" : "author 1",
-        },
-        {
-        "title": "Blog 2",
-        "content" : "content 2",
-        "author" : "author 2",
-        },
-        {
-        "title": "Blog 3",
-        "content" : "content 3",
-        "author" : "author 3",
-        },
-        {
-        "title": "Blog 4",
-        "content" : "content 4",
-        "author" : "author 4",
-        }
-    ]
-
-@app.route('/')
+# Veritabanı bağlantısını kurma
+connection = pymysql.connect(
+    host='localhost',
+    user='root',
+    password='12345',
+    db='movie',
+    port=3308,
+    charset='utf8mb4',
+    cursorclass=pymysql.cursors.DictCursor
+)
+@app.route('/movies/')
 def index():
-    return "Hello, world"
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM Movie_info")
+        movies = cursor.fetchall()
+    return render_template('index.html', movies=movies)
 
-@app.route('/about/')
-def about_page():
-    return "This is about page"
-
-@app.route('/blogs/')
-def blogs():
-    return blog_list
-@app.route('/blogs/<int:id>')
-def blog_detail(id):
-    if id > 0 and id <= len(blog_list):
-        return blog_list[id-1]
-    else:
-        return "Blog not found!"
-@app.route('/blogs/<string:blog_title>')
-def blog_detail_with_string(blog_title):
-    return f"Blog with title {blog_title}"
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/movies/<int:movie_id>')
+def movie_detail(movie_id):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM Movie_info WHERE id = %s", (movie_id,))
+        movie = cursor.fetchone()
+    if movie is None:
+        return "Movie not found", 404
+    return render_template('movie.html', movie=movie)
